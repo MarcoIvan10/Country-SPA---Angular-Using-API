182d49779a31a9c40c1612a762e4f538ce7c0232
MarcoIvan <marcoivanf10@gmail.com>
2025-02-10 19:17:56 -0600

    Creación del componente loading-spinner

---
+++ src/app/shared/components/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-spinner',
+  standalone: false,
+  templateUrl: './loading-spinner.component.html',
+  styles: ``
+})
+export class LoadingSpinnerComponent {
+
+}
